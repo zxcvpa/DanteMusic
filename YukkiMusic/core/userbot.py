a4e37d9a6562d6c67eb04bc9c8b4ef275d754349
DANTE <104479517+xflydan@users.noreply.github.com>
2023-11-01 15:35:49 +0700

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,9 +57,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DanteSupportss")
-                await self.one.join_chat("DantePedia")
-                await self.one.join_chat("ab0utd4n")
+                await self.five.join_chat("ab0utd4n")
+                await self.five.join_chat("DantePedia")
+                await self.five.join_chat("DanteSupportss")
             except:
                 pass
             assistants.append(1)
@@ -88,9 +88,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DanteSupportss")
-                await self.two.join_chat("DantePedia")
-                await self.two.join_chat("ab0utd4n")
+                await self.five.join_chat("ab0utd4n")
+                await self.five.join_chat("DantePedia")
+                await self.five.join_chat("DanteSupportss")
             except:
                 pass
             assistants.append(2)
@@ -119,9 +119,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.two.join_chat("DanteSupportss")
-                await self.two.join_chat("DantePedia")
-                await self.two.join_chat("ab0utd4n")
+                await self.five.join_chat("ab0utd4n")
+                await self.five.join_chat("DantePedia")
+                await self.five.join_chat("DanteSupportss")
             except:
                 pass
             assistants.append(3)
@@ -150,9 +150,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                 await self.two.join_chat("DanteSupportss")
-                await self.two.join_chat("DantePedia")
-                await self.two.join_chat("ab0utd4n")
+                await self.five.join_chat("ab0utd4n")
+                await self.five.join_chat("DantePedia")
+                await self.five.join_chat("DanteSupportss")
             except:
                 pass
             assistants.append(4)
@@ -181,9 +181,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.two.join_chat("DanteSupportss")
-                await self.two.join_chat("DantePedia")
-                await self.two.join_chat("ab0utd4n")
+                await self.five.join_chat("ab0utd4n")
+                await self.five.join_chat("DantePedia")
+                await self.five.join_chat("DanteSupportss")
             except:
                 pass
             assistants.append(5)
